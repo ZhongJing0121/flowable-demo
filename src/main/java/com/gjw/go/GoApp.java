46c91d2dd5628fcf0bfd03ee02abda46f53d375b
zhongjing <zhongjing0121@aliyun.com>
2024-01-08 16:01:39 +0800

    Initial commit

---
+++ src/main/java/com/gjw/go/GoApp.java
@@ -0,0 +1,12 @@
+package com.gjw.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoApp {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GoApp.class);
+        app.run(args);
+    }
+}
